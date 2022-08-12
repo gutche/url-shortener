a4ce926349b4977ae785db69e8d9c6036fbc55ae
gutche <gledrianregalagutierrez@gmail.com>
2022-08-12 12:49:45 +0200

    1st upload

--- index.js
+++ index.js
@@ -2,23 +2,58 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const bodyParser = require('body-parser');
+const dns = require('dns');
+const urlParser = require('url-parse');
 
 // Basic Configuration
-const port = process.env.PORT || 3000;
+const port = 3000;
 
+mongoose.connect(process.env.DATABASE_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+urlSchema = new Schema({
+  url: String,
+});
+
+const Url = new mongoose.model('Url', urlSchema);
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post(
+  '/api/shorturl/',
+  bodyParser.urlencoded({ extended: false }),
+  (req, res) => {
+    const { url } = req.body;
+    const hostname = new urlParser(url).hostname;
+
+    dns.lookup(hostname, (err) => {
+      if (err) res.json({ error: 'Invalid URL' });
+
+      const newUrl = new Url({ url });
+      newUrl.save();
+      res.json({ original_url: newUrl.url, short_url: newUrl.id });
+    });
+  }
+);
+
+app.get('/api/shorturl/:id', (req, res) => {
+  const { id } = req.params;
+  Url.findById(id, (err, data) => {
+    if (err) res.json({ error: 'Invalid URL' });
+    res.redirect(data.url);
+  });
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
